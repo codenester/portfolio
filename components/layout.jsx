68527a30d5306b6445e7e137f34f74325c10afe8
codenester <srrthea@gmail.com>
2022-11-29 11:59:05 +0700

    setting and theme

--- components/layout.jsx
+++ components/layout.jsx
@@ -2,27 +2,39 @@ import TopNavBar from './top-nav-bar'
 import NavBarDrawer from './nav-bar-drawer'
 import Setting from './setting'
 import { useCookies } from 'react-cookie'
-import {useEffect} from 'react'
-export default function Layout({children}){
-    const [cookie,setCookie]=useCookies(["rithea-theme","rithea-lang"])
-    useEffect(()=>{
+import { useEffect, useState } from 'react'
+import { LangContext, ThemeContext } from '../contexts'
+export default function Layout({ children }) {
+    const [cookie, setCookie] = useCookies(["rithea-theme", "rithea-lang"])
+    const [theme, setTheme] = useState(cookie['rithea-theme'] ?? "dark")
+    const [lang, setLang] = useState(cookie['rithea-lang'] ?? 'en')
+    useEffect(() => {
         const d = new Date();
-        d.setDate(d.getDate()+14)
-        if(!cookie['rithea-theme']) setCookie("rithea-theme","light",{
-            expires:d,
-            path:'/'
-        })
-        if(!cookie['rithea-lang']) setCookie("rithea-lang","en",{
-            expires:d,
-            path:'/'
-        })
-        console.log(cookie['rithea-theme'])
+        d.setDate(d.getDate() + 14)
+        if (!cookie['rithea-theme']) {
+            setCookie("rithea-theme", "dark", {
+                expires: d,
+                path: '/'
+            })
+        }
+        if (!cookie['rithea-lang']) {
+            setCookie("rithea-lang", "en", {
+                expires: d,
+                path: '/'
+            })
+        }
     },)
-    return <div className={`container ${cookie['rithea-theme']}`}>
-        <TopNavBar/>
-        <NavBarDrawer/>
-        <Setting/>
-        {children}
-        <p>Published on {new Date().toDateString()}</p>
-    </div>
+    return (
+        <LangContext.Provider value={{ lang, setLang }}>
+            <ThemeContext.Provider value={{ theme, setTheme }}>
+                <div className={`container ${theme}`}>
+                    <TopNavBar />
+                    <NavBarDrawer />
+                    <Setting />
+                    {children}
+                    <p>Published on {new Date().toDateString()}</p>
+                </div>
+            </ThemeContext.Provider>
+        </LangContext.Provider>
+    )
 }
--- components/setting.jsx
+++ components/setting.jsx
@@ -1,5 +1,17 @@
-export default function Setting(){
-    return <div>
-        Setting
+import { useContext } from "react"
+import { LangContext, ThemeContext } from "../contexts"
+
+export default function Setting() {
+    const { theme, setTheme } = useContext(ThemeContext)
+    const { lang, setLang } = useContext(LangContext)
+    return <div className="float-setting">
+        <button>
+            <i class="ri-moon-fill"></i>
+            {theme}
+        </button>
+        <button>
+            <i class="ri-translate-2"></i>
+            {lang}
+        </button>
     </div>
 }
